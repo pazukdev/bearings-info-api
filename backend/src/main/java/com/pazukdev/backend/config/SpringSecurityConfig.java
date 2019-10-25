bd25b1462df31221c2cf7cfc01a60e3245a9b3da
Siarhei Sviarkaltsau <pazuk1985@gmail.com>
2019-10-25 10:15:49 +0300

    update

--- backend/src/main/java/com/pazukdev/backend/config/SpringSecurityConfig.java
+++ backend/src/main/java/com/pazukdev/backend/config/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import com.pazukdev.backend.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -28,6 +29,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .cors().and()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .antMatchers("/css/**", "/index", "/js/**").permitAll()
                 .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
                 .antMatchers("/test/public").permitAll()
