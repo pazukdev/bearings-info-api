8b1630cf98d50ec4c2083ecea9aedf9a67a539b7
Siarhei Sviarkaltsau <pazuk1985@gmail.com>
2019-10-27 19:09:31 +0300

    update

--- backend/src/main/java/com/pazukdev/backend/entity/AbstractEntity.java
+++ backend/src/main/java/com/pazukdev/backend/entity/AbstractEntity.java
@@ -2,7 +2,11 @@ package com.pazukdev.backend.entity;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -13,7 +17,7 @@ import javax.validation.constraints.NotNull;
 public abstract class AbstractEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Long id;
     @NotNull
