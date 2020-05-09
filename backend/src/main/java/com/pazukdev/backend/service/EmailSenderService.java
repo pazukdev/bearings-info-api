94cd23dc8224e68a67139b261a361bf5936204c9
pazukdev <pazukdev@gmail.com>
2020-05-09 17:15:01 +0300

    JavaMailSender: SendGrid

--- backend/src/main/java/com/pazukdev/backend/service/EmailSenderService.java
+++ backend/src/main/java/com/pazukdev/backend/service/EmailSenderService.java
@@ -1,11 +1,20 @@
 package com.pazukdev.backend.service;
 
 import com.pazukdev.backend.constant.Constant;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 /**
  * @author Siarhei Sviarkaltsau
@@ -47,14 +56,37 @@ public class EmailSenderService {
         emailTo(Constant.EMAIL, subject, text, false);
     }
 
-    public void emailTo(final String toAddress, final String subject, final String text, final boolean html) {
-        final SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom(Constant.EMAIL);
-        mailMessage.setTo(toAddress);
-        mailMessage.setSubject(subject);
-        mailMessage.setText(text);
+    public void emailTo(final String toAddress,
+                        final String subject,
+                        final String text,
+                        final boolean html) {
+//        final SimpleMailMessage mailMessage = new SimpleMailMessage();
+//        mailMessage.setFrom(Constant.EMAIL);
+//        mailMessage.setTo(toAddress);
+//        mailMessage.setSubject(subject);
+//        mailMessage.setText(text);
+//
+//        send(mailMessage);
 
-        send(mailMessage);
+        Email from = new Email(Constant.EMAIL);
+        Email to = new Email(toAddress);
+        Content content = new Content("text/plain", text);
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 //    public void emailTo(final String toAddress,
